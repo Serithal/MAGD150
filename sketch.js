6c47cabd9f4968165b3a8a9a54d63dfb94ab9977
Serithal <82673547+Serithal@users.noreply.github.com>
2021-04-21 19:23:47 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,82 @@
-var cx, cy;
-var rx = 350;
-var ry = 350;
-var rw = 40;
-var rh = 40;
-var cx = 60;
-var cy = 310;
-var cwspeed = 2;
-var chspeed = 1;
-var EFILLG = 50;
-var EFILLR = 50;
-var EFILLB = 50;
-var TFILLR = 100;
-var TFILLG = 100;
-var TFILLB = 100;
-// everytime I tried to program a circular button the entire program failed so I will have to skip that unfortunately
+var rectx = 0
+var recty = 0
+var recth = 20
+var rectw = 20
+var rpm = 0
+var rpml = 0
+cb = 0
+cr = 0
+cg = 0
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+   randomizeNumbers();
 }
 
 function draw() {
-  background(100, 50, 10);
-  //for rectangle button
-  push();
-  rect(rx, ry, rw, rh);
-  fill(200, 30, 30);
-       push();
-textSize(10);
-  fill(0, 0, 100);
-      text('Push me', 350, 345);
-      pop();
-  if(mouseX>rx && mouseX<rx+rw && mouseY>ry && mouseY<ry+rh){fill(90,100,30);} //sqaure highlight
+  background(100, 100, 200);
+  BigBox();
+  checkMouse();
+  randomColors();
+}
+
+function BigBox(){
+  fill(20, 100, 100);
+  rect(rectx, recty, rectw, recth);
+}
+
+function randomColors(){
+  cb += 1.5
+  cr += 1
+  cg += 2
+  if (cb >= 255){
+    cb=1
+  }
+  if (cr >= 235){
+    cr = 1
+  }
+  if (cg >= 215){
+    cg = 1
+  }
+}
+
+function randomizeNumbers(){
+  rectx += random(1, 780);
+  recty += random(1, 780);
+  recth += random(1, 20);
+  rectw += random(1, 20);
+}
+
+function checkMouse(){
+  if(mouseX>rectx && mouseX<rectx+rectw && mouseY>recty && mouseY<recty+recth){
+          fill(100, 200, 100);
+      rect(rectx, recty, rectw, recth);
+  }
   if(mouseIsPressed){
-    if(mouseX>rx && mouseX<rx+rw && mouseY>ry && mouseY<ry+rh){
-      fill(30, 200, 30);
+    if(mouseX>rectx && mouseX<rectx+rectw && mouseY>recty && mouseY<recty+recth){
+      fill(20, 50, 50);
+      rect(rectx, recty, rectw, recth);
+      translate(400, 400);
+      drawPakman();
       push();
-      fill(TFILLR,TFILLG, TFILLB);
-      rect(30, 30, 340, 300);
+      scale(2);
+      drawPakmanColor();
+      rpml += 0.01
       pop();
-      push();
-      fill(EFILLR, EFILLG, EFILLB);
-      ellipse(cx, cy, 30, 30);
-      cx+= cwspeed;
-      cy-= chspeed;
-      
-      if(cy<=45){chspeed = -1; EFILLG = 250; EFILLB = 200;}
-      if(cy>=315){chspeed= 1; EFILLR = 230; EFILLB = 30;}
-      if(cx>=355){cwspeed = -2; EFILLB = 200; EFILLR = 30;}
-      if(cx<=45){cwspeed = 2; EFILLG = 50; EFILLR = 230;}
-      pop();
-      // for bouncing circle 
- 
-      
     }
   }
-
 }
+
+function drawPakman(){
+  fill(120, 200, 20);
+   rotate(PI - rpm);
+  rpm += 0.1
+arc(100, 100, 90, 90, 0, PI + QUARTER_PI, PIE);
  
+}
+
+function drawPakmanColor(){
+  fill(cr, cg, cb);
+   rotate(PI - rpml);
+arc(100, 100, 90, 90, 0, PI + QUARTER_PI, PIE);
+ 
+}
