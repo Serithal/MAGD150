1069daa8e1706cec79aaefb3ba172381fe1749b2
Serithal <82673547+Serithal@users.noreply.github.com>
2021-04-21 19:24:41 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,82 +1,78 @@
-var rectx = 0
-var recty = 0
-var recth = 20
-var rectw = 20
-var rpm = 0
-var rpml = 0
-cb = 0
-cr = 0
-cg = 0
+let snow1;
+//The array holding the different text options
+let array = ["Its Snowing", "So Cold", "Bring A Coat"];
+var check = 0;
 function setup() {
-  createCanvas(800, 800);
-   randomizeNumbers();
+  createCanvas(400, 400);
+  snow1 = new funny();
 }
-
+// All of the function calls bellow are telling the program to snow harder
 function draw() {
-  background(100, 100, 200);
-  BigBox();
-  checkMouse();
-  randomColors();
-}
+  //noLoop();
+  background(100);
+   snow1.snowfall();
+  snow1.drawsnow();
+   snow1.snowfall();
+  snow1.drawsnow();
+   snow1.snowfall();
+  snow1.drawsnow();
+   snow1.snowfall();
+  snow1.drawsnow();
+   snow1.snowfall();
+  snow1.drawsnow();
+   snow1.snowfall();
+  snow1.drawsnow();
+    snow1.snowfall();
+  snow1.drawsnow();
+    snow1.snowfall();
+  snow1.drawsnow();
+    snow1.snowfall();
+  snow1.drawsnow();
+    snow1.snowfall();
+  snow1.drawsnow();
+    snow1.snowfall();
+  snow1.drawsnow();
+    snow1.snowfall();
+  snow1.drawsnow();
+      fill(255);
+    textSize(40);
+    text(array[check], 200, 350);
 
-function BigBox(){
-  fill(20, 100, 100);
-  rect(rectx, recty, rectw, recth);
+  
 }
-
-function randomColors(){
-  cb += 1.5
-  cr += 1
-  cg += 2
-  if (cb >= 255){
-    cb=1
-  }
-  if (cr >= 235){
-    cr = 1
-  }
-  if (cg >= 215){
-    cg = 1
-  }
-}
-
-function randomizeNumbers(){
-  rectx += random(1, 780);
-  recty += random(1, 780);
-  recth += random(1, 20);
-  rectw += random(1, 20);
-}
-
-function checkMouse(){
-  if(mouseX>rectx && mouseX<rectx+rectw && mouseY>recty && mouseY<recty+recth){
-          fill(100, 200, 100);
-      rect(rectx, recty, rectw, recth);
-  }
-  if(mouseIsPressed){
-    if(mouseX>rectx && mouseX<rectx+rectw && mouseY>recty && mouseY<recty+recth){
-      fill(20, 50, 50);
-      rect(rectx, recty, rectw, recth);
-      translate(400, 400);
-      drawPakman();
-      push();
-      scale(2);
-      drawPakmanColor();
-      rpml += 0.01
-      pop();
+//When the mouse is pressed a different text will display
+  function mousePressed(){
+check = check + 1;
+    if(check == 3){
+      check =0;
     }
   }
-}
 
-function drawPakman(){
-  fill(120, 200, 20);
-   rotate(PI - rpm);
-  rpm += 0.1
-arc(100, 100, 90, 90, 0, PI + QUARTER_PI, PIE);
- 
-}
+//the snow class with all the variables necessary
+class funny {
+  
+  Constructor(){
+    var T, R, B, V, K;
+    this.T = random(0, width);
+    this.R = random(0, height);
+    this.B = random(4, 10);
+    this.V = random(4, 10);
+    this.K = 1;
+  }
+  
+  drawsnow(){
+    fill(255);
+    ellipse(this.T, this.R, this.B, this.V);
+    
+  }
+  snowfall(){
+    this.T = random(0, width);
+    this.R = random (0, height);
+    this.B = random(4, 10);
+    this.V = random(4, 10);
 
-function drawPakmanColor(){
-  fill(cr, cg, cb);
-   rotate(PI - rpml);
-arc(100, 100, 90, 90, 0, PI + QUARTER_PI, PIE);
- 
+    if(this.R > height){
+      this.R = 1;
+    }
+  }
 }
